7a8ff07c13d2c3d5d69f4801cbeae1f0e42b4f5c
Kyle Mulqueen <kmulqdrums710@gmail.com>
2020-03-20 21:51:53 -0600

    deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const routes = require("./routes");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,5 +12,16 @@ connectDB();
 
 // Routes
 app.use(routes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // Serve html file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
